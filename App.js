f45742870f1181f7b06a64e6cd268f366fae74ca
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-19 17:05:21 +0100

    add todo

--- App.js
+++ App.js
@@ -1,29 +1,51 @@
-import { StyleSheet, Text, View ,  KeyboardAvoidingView, Platform, TextInput , TouchableOpacity} from 'react-native';
-import Item from './components/Item';
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import Item from "./components/Item";
+import { useState } from "react";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  // add item
+  const handlePress = () => {
+    setItems([...items, input]);
+    setInput("");
+  };
+
   return (
     <View style={styles.container}>
-      
       {/* Today's Tasks */}
       <View style={styles.wrapper}>
-        <Text style={styles.sectionTitle}>
-          Today's Task
-        </Text>
+        <Text style={styles.sectionTitle}>Today's Task</Text>
         <View style={styles.items}>
-          <Item />
-          <Item />
+          {items.map((item, index) => {
+            return <Item text={item} key={index} />;
+          })}
         </View>
       </View>
 
       {/* Input */}
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.writeTodo}>
-        <TextInput style={styles.input} placeholder="Add todo!" />
-        <TouchableOpacity>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTodo}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Add todo!"
+          onChangeText={(text) => setInput(text)}
+          value={input}
+        />
+        <TouchableOpacity onPress={() => handlePress()}>
           <View style={styles.addTodo}>
-            <Text style={styles.addText}>
-              +
-            </Text>
+            <Text style={styles.addText}>+</Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
@@ -34,45 +56,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED'
+    backgroundColor: "#E8EAED",
   },
-  wrapper:{
-    paddingTop:80,
-    paddingHorizontal:20,
-
+  wrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
-  sectionTitle:{
-    fontSize:24,
-    fontWeight:'bold'
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
-  items:{
-    marginTop:30
+  items: {
+    marginTop: 30,
   },
-  writeTodo:{
-      position:'absolute',
-      bottom:60,
-      width:"100%",
-      flexDirection:'row',
-      justifyContent:'space-around',
-      alignItems:'center'
+  writeTodo: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
   },
-  input:{
-      width:'100%',
-      maxWidth:250,
-      padding:15,
-      backgroundColor:'#FFF',
-      borderRadius:60,
-      borderColor:'#C0C0C0'
+  input: {
+    width: "100%",
+    maxWidth: 250,
+    padding: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
   },
-  addTodo:{
-    width:60 ,
-    height:60,
-    backgroundColor:'white',
-    borderRadius:60,
-    alignItems:'center',
-    justifyContent:'center'
+  addTodo: {
+    width: 60,
+    height: 60,
+    backgroundColor: "white",
+    borderRadius: 60,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  addText:{
-
-  }
+  addText: {},
 });

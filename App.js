770467d2241155f6e853ee24b0411e89b8aafa06
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-19 16:52:46 +0100

    input container

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,  KeyboardAvoidingView, Platform, TextInput , TouchableOpacity} from 'react-native';
 import Item from './components/Item';
 
 export default function App() {
@@ -6,7 +6,6 @@ export default function App() {
     <View style={styles.container}>
       
       {/* Today's Tasks */}
-
       <View style={styles.wrapper}>
         <Text style={styles.sectionTitle}>
           Today's Task
@@ -16,6 +15,18 @@ export default function App() {
           <Item />
         </View>
       </View>
+
+      {/* Input */}
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.writeTodo}>
+        <TextInput style={styles.input} placeholder="Add todo!" />
+        <TouchableOpacity>
+          <View style={styles.addTodo}>
+            <Text style={styles.addText}>
+              +
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -36,5 +47,32 @@ const styles = StyleSheet.create({
   },
   items:{
     marginTop:30
+  },
+  writeTodo:{
+      position:'absolute',
+      bottom:60,
+      width:"100%",
+      flexDirection:'row',
+      justifyContent:'space-around',
+      alignItems:'center'
+  },
+  input:{
+      width:'100%',
+      maxWidth:250,
+      padding:15,
+      backgroundColor:'#FFF',
+      borderRadius:60,
+      borderColor:'#C0C0C0'
+  },
+  addTodo:{
+    width:60 ,
+    height:60,
+    backgroundColor:'white',
+    borderRadius:60,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  addText:{
+
   }
 });
